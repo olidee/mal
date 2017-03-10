2dfd81e503d8e74846bf81008f0edd958686ab75
olidee <oli.desborough@ipl.com>
2017-03-10 11:58:57 +0000

    Got fn* working

--- oli-clj/src/mal/env.clj
+++ oli-clj/src/mal/env.clj
@@ -1,18 +1,21 @@
 (ns mal.env)
 
-(defn create-env [outer]
-  (atom {:env {} :outer outer}))
+(defn- find-env-for [env k]
+  (if (contains? (:env @env) k)
+    env
+    (if-let [outer (:outer @env)]
+      (find-env-for outer k)
+      (throw (Exception. (str "Symbol '" k "' not found"))))))
+
+(defn create-env
+  ([outer] (atom {:env {} :outer outer}))
+  ([outer binds exprs]
+   (let [env (zipmap binds exprs)]
+     (atom {:env env :outer outer}))))
 
 (defn set-in-env [env k v]
   (swap! env assoc-in [:env k] v)
   v)
 
 (defn get-from-env [env k]
-  (get-in @env [:env k]))
-
-(defn find-env-for [env k]
-  (if (contains? (:env @env) k)
-    env
-    (if-let [outer (:outer @env)]
-      (find-env-for outer k)
-      (throw (Exception. (str "Symbol '" k "' not found"))))))
+  (get-in @(find-env-for env k) [:env k]))
--- oli-clj/src/mal/eval.clj
+++ oli-clj/src/mal/eval.clj
@@ -8,12 +8,12 @@
 
 (defn- eval-node [n env]
   (cond
-    (string? n) (mal.env/get-from-env (mal.env/find-env-for env n) n)
+    (string? n) (mal.env/get-from-env env n)
     (vector? n) (map #(eval-expr % env) n)
     :else n))
 
 (defn- maldef! [name value env]
-  (mal.env/set-in-env env name (eval-node value env)))
+  (mal.env/set-in-env env name (eval-expr value env)))
 
 (defn- mallet* [bindings expr env]
   (let [bindings (apply hash-map bindings)
@@ -22,13 +22,32 @@
       (mal.env/set-in-env new-env k (eval-expr v new-env)))
     (eval-expr expr new-env)))
 
+(defn- malfn* [params body env]
+  (if (vector? params)
+    (fn [& args]
+      (if (= (count params) (count args))
+        (let [fn-env (mal.env/create-env env params args)]
+          (eval-expr body fn-env))
+        (throw (Exception. "Wrong number of args passed to fn"))))
+    (throw (Exception. "Function definition should be of the form (fn* (args) (body))"))))
+
+(defn- malif [test then else env]
+  (let [body (if (eval-expr test env) then else)]
+    (eval-expr body env)))
+
+(defn- maldo [expr env]
+  (last (eval-ast expr env)))
+
 (defn- eval-expr [expr env]
   (cond
     (not (vector? expr)) (eval-node expr env)
     (empty? expr) expr
-    :else (let [[fn-name arg1 arg2] expr]
+    :else (let [[fn-name arg1 arg2 arg3] expr]
             (case fn-name
               "def!" (maldef! arg1 arg2 env)
               "let*" (mallet* arg1 arg2 env)
+              "fn*"  (malfn* arg1 arg2 env)
+              "if"   (malif arg1 arg2 arg3 env)
+              "do"   (maldo arg1 env)
               (let [[f & args] (eval-node expr env)]
                 (apply f args))))))
